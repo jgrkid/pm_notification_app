2bf69acc3d18ed7493f256694d492ea288bb9257
Josef Graf <josef.graf@4process.de>
2024-11-13 13:21:06 +0000

    Implemented UploadSet in View

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,18 +1,35 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+	"sap/m/library",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/Item",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/upload/Uploader"
 ],
-    function (Controller) {
+    function (MobileLibrary, Controller, Item, JSONModel, Uploader) {
         "use strict";
-
+        
         return Controller.extend("pmnotificationapp.controller.Main", {
             onInit: function () {
 
+                // Routing
                 this.oOwnerComponent = this.getOwnerComponent();
                 this.oRouter = this.oOwnerComponent.getRouter();
                 this.oRouter.getRoute("RouteMain").attachPatternMatched(this.onRouteMatched, this);
+
+
+                // //Init Uploader
+                 var oUploadSet = this.byId("UploadSet")
+
+			    // Modify "add file" button
+                oUploadSet.getDefaultFileUploader().setButtonOnly(false)
+			    oUploadSet.getDefaultFileUploader().setTooltip("")
+			    oUploadSet.getDefaultFileUploader().setIconOnly(true)
+			    oUploadSet.getDefaultFileUploader().setIcon("sap-icon://attachment")
+
             },
 
 
+            
             onRouteMatched: function (oEvent) {
                 var oModel = this.getView().getModel()
                 var that = this
@@ -24,6 +41,28 @@ sap.ui.define([
                         
                         that.getView().setBindingContext(oContext);
                     })
-            }
+            },
+
+            // onUploadSelectedButton: function () {
+            //     var oUploadSet = this.byId("UploadSet");
+
+            //     oUploadSet.getItems().forEach(function (oItem) {
+            //         if (oItem.getListItem().getSelected()) {
+            //             oUploadSet.uploadItem(oItem);
+            //         }
+            //     });
+            // },
+            // onDownloadSelectedButton: function () {
+            //     var oUploadSet = this.byId("UploadSet");
+    
+            //     oUploadSet.getItems().forEach(function (oItem) {
+            //         if (oItem.getListItem().getSelected()) {
+            //             oItem.download(true);
+            //         }
+            //     });
+            // },
+            // onUploadCompleted: function(oEvent) {
+            //     this.oItemToUpdate = null;
+            // }
         });
     });
